d384256a1902277cad135c38d58ca963c575f0ad
sonintact <hktts474941@gmail.com>
2024-09-25 16:49:15 +0700

    update

--- src/actions/config/get.ts
+++ src/actions/config/get.ts
@@ -6,21 +6,25 @@ const config_ignores = new Set(['withdraw']);
 export default async (_: UserWithNonce, data: Record<string, any>, replyMessage: (return_action: string, data: Record<string, any>) => void) => {
     let response_config: Record<string, any> = {};
 
-    if (data.projection === '*') {
-        const all_config = await redisWrapper.get_all('config');
+    try {
+        if (data.projection === '*') {
+            const all_config = await redisWrapper.get_all('config');
 
-        response_config = Object.values(all_config).map(i => Object.entries(i)).reduce((acc, config) => {
-            const [key, value] = [config[0][0], config[0][1]];
+            response_config = Object.values(all_config).map(i => Object.entries(i)).reduce((acc, config) => {
+                const [key, value] = [config[0][0], config[0][1]];
 
-            if (config_ignores.has(key)) return acc;
+                if (config_ignores.has(key)) return acc;
 
-            delete value._id;
+                delete value._id;
 
-            delete value.config_type;
+                delete value.config_type;
 
-            return { ...acc, [key]: value };
-        }, {} as Record<string, any>);
+                return { ...acc, [key]: value };
+            }, {} as Record<string, any>);
+        };
+
+        replyMessage(data.return_action, response_config);
+    } catch (error) {
+        console.error(error);
     };
-
-    replyMessage(data.return_action, response_config);
 }
--- src/actions/self/get.ts
+++ src/actions/self/get.ts
@@ -3,22 +3,26 @@ import type { UserWithNonce } from "../../types";
 
 export default async (ws_user: UserWithNonce, data: Record<string, any>, replyMessage: (return_action: string, data: Record<string, any>) => void) => {
     if (typeof data.projection !== 'string') return;
-    
-    const user = await redisWrapper.get("users", ws_user.tele_id) as Record<string, any>;
 
-    delete user._id;
+    try {
+        const user = await redisWrapper.get("users", ws_user.tele_id) as Record<string, any>;
 
-    let response_user: Record<string, any> = {};
+        delete user._id;
 
-    if (data.projection === '*') {
-        response_user = { ...user };
-    } else {
-        const porjections = data.projection.split(' ');
+        let response_user: Record<string, any> = {};
 
-        for (const projection of porjections) {
-            response_user[projection] = user[projection];
+        if (data.projection === '*') {
+            response_user = { ...user };
+        } else {
+            const porjections = data.projection.split(' ');
+
+            for (const projection of porjections) {
+                response_user[projection] = user[projection];
+            };
         };
+        
+        replyMessage(data.return_action, response_user);
+    } catch (error) {
+        console.error(error);
     };
-    
-    replyMessage(data.return_action, response_user);
 }
